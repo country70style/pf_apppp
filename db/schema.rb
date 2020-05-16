33858432b871db372c64be192c527ca1a6994ed3
country70style <teppei.h1225@gmail.com>
2020-05-16 13:48:43 +0900

    make like model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_25_033620) do
+ActiveRecord::Schema.define(version: 2020_05_16_043917) do
+
+  create_table "likes", force: :cascade do |t|
+    t.integer "tweet_id", null: false
+    t.string "ip"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["tweet_id"], name: "index_likes_on_tweet_id"
+  end
 
   create_table "perks", force: :cascade do |t|
     t.string "name"
@@ -38,4 +46,5 @@ ActiveRecord::Schema.define(version: 2020_04_25_033620) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "likes", "tweets"
 end
