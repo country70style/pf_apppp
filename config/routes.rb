40f16e3eab4ce5bea31bc6e4d1ac303bbf7de948
country70style <teppei.h1225@gmail.com>
2020-05-16 13:53:59 +0900

    edit routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
   resources :weapons
   resources :posts 
   root to: 'posts#index'
+  resources :tweets do
+    resources :likes, only: [:create]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
